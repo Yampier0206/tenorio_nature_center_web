448ff74dbf6d93a455b30de9647bea0860db8185
Eddier López <lopez.eddier@gmail.com>
2026-05-14 16:57:22 -0600

    Codigo inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import{Home} from './components/home/home'
+import { Error } from './components/error/error';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:Home},
+    {path:'login',component:Login},
+    {path:'**',component:Error}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { CategoryService } from './services/category.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  public categories:any
   protected readonly title = signal('webapp');
+  constructor(private categoryService:CategoryService){
+    this.loadCategories()
+  }
+  loadCategories(){
+    this.categoryService.getCategories().subscribe({
+      next:(response:any)=>{
+        console.log('Respuesta---->',response)
+      },
+      error:(err:Error)=>{
+        console.log('Error---->',err)
+      }
+    })
+  }
+
 }
