569e6e180c896c68d2bb826a4e0576eb2beb9d44
diego <diego.ccc77@gmail.com>
2026-06-08 23:07:01 -0600

    feat:pt2(no vi que era del mismo)

--- src/app/services/idioma-guia.service.ts
+++ src/app/services/idioma-guia.service.ts
@@ -1,25 +1,46 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { enviroment } from '../enviroments';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { enviroment } from "../enviroments";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class IdiomaGuiaService {
+    private url: string;
 
-  private url:string;
+    constructor(private _http: HttpClient) {
+        this.url = enviroment.apiUrl;
+    }
 
-  constructor(private http:HttpClient){
-    this.url = enviroment.apiUrl;
-  }
+    private getHeaders(): HttpHeaders {
+        const token = sessionStorage.getItem('token');
+        return new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        });
+    }
 
-  getIdiomasByGuia(id:number):Observable<any>{
-
-    return this.http.get(
-      this.url + 'idiomaguia/guia/' + id
+    getIdiomasByGuia(idGuia: number): Observable<any> {
+        return this._http.get(
+            this.url + 'idiomaguia/guia/' + idGuia,
+            { headers: this.getHeaders() }
+        );
+    }
+    addIdiomaToGuia(idGuia: number, idIdioma: number): Observable<any> {
+    const payload = { 
+        idguia: Number(idGuia), 
+        ididioma: Number(idIdioma) 
+    };
+        return this._http.post(
+        this.url + 'idiomaguia',
+        payload,
+        { headers: this.getHeaders() }
     );
+}
 
-  }
-
+    deleteIdiomaFromGuia(idIdiomaguia: number): Observable<any> {
+        return this._http.delete(
+            this.url + 'idiomaguia/' + idIdiomaguia,
+            { headers: this.getHeaders() }
+        );
+    }
 }
